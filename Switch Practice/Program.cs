84237cbd5835c174f2862e3423c34183e6080200
Eric Thomas <eric.therock@gmail.com>
2018-07-03 07:14:40 -0500

    Basic switch statement

--- Switch Practice/Program.cs
+++ Switch Practice/Program.cs
@@ -10,6 +10,31 @@ namespace Switch_Practice
     {
         static void Main(string[] args)
         {
+            Console.Write("First number please: ");
+            string numberOneAsText = Console.ReadLine();
+            int numberOne = Convert.ToInt32(numberOneAsText);
+
+            Console.Write("Second number please: ");
+            string numberTwoAsText = Console.ReadLine();
+            int numberTwo = Convert.ToInt32(numberTwoAsText);
+
+            Console.Write("Enter the type of operation you want to perform: ");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    var additionresult = numberOne + numberTwo;
+                    Console.WriteLine(additionresult);
+                    Console.ReadLine();
+                    break;
+
+                case "-":
+                    var subtractionresult = numberOne - numberTwo;
+                    Console.WriteLine(subtractionresult);
+                    Console.ReadLine();
+                    break;
+            }
         }
     }
 }
